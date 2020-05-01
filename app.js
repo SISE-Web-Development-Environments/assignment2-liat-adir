b78ff293008e45adfdca9a7037ff53217fb9ac52
AdirBiran <adir.biran@gmail.com>
2020-05-01 21:56:20 +0300

    changed screen

--- app.js
+++ app.js
@@ -16,6 +16,18 @@ var maxMonsters;
 var health;
 var gameTime;
 var timeBonusAvailable;
+var up = 38;
+var down = 40;
+var left = 37;
+var right = 39;
+var smallBallColor;
+var mediumBallColor;
+var largeBallColor;
+var base_size = 40;
+var rows = 25;
+var columns = 12;
+var remainingBalls;
+
 
 $(document).ready(function() {
 	context = canvas.getContext("2d");
@@ -32,6 +44,73 @@ $(document).ready(function() {
 // 9 - health (+1 life)
 // 10 - timeBonus (+10)
 
+function initButtons(upKey, downKey, leftKey, rightKey)
+{
+	up = upKey;
+	down = downKey;
+	left = leftKey;
+	right = rightKey;
+}
+
+function setGameTime(time)
+{
+	if (time >= 60)
+		gameTime = time;
+	else
+		gameTime = 60;
+}
+
+function setMobs(mobsNumber)
+{
+	maxMonsters = mobsNumber;
+}
+
+function setBallsColors(small, medium, large)
+{
+	smallBallColor = small;
+	mediumBallColor = medium;
+	largeBallColor = large;
+}
+
+function setBallsNumber(ballsNumber)
+{
+	if (ballsNumber <= 90 && ballsNumber >= 50)
+		food_remain = ballsNumber;
+	else
+		food_remain = 70;
+}
+
+function randomizeValues()
+{
+	var colors = ["red", "green", "blue", "black", "purple"];
+	var used = [0,0,0,0,0];
+
+	var rndTime = Math.floor(Math.random()*60+61); // between 60 and 120
+	var rndBallsNumber = Math.floor(Math.random()*40+51);
+	var rndMobs = Math.floor(Math.random()*4 + 1);
+
+	var rndBallsColors = new Array();
+
+	var rndColor = Math.floor(Math.random()*5);
+
+	for (var i = 0; i < 3; i++)
+	{
+		while (used[rndColor] == 1)
+			rndColor = Math.floor(Math.random()*5);
+
+		rndBallsColors[i] = colors[rndColor];
+		used[rndColor] = 1;
+	}
+
+	maxMonsters = rndMobs;
+	gameTime = rndTime;
+	food_remain = rndBallsNumber;
+	smallBallColor = rndBallsColors[0];
+	mediumBallColor = rndBallsColors[1];
+	largeBallColor = rndBallsColors[2];
+
+}
+
 function Start() {
 	board = new Array();
 	score = 0;
@@ -40,21 +119,22 @@ function Start() {
 	lastKeyPressed = "right";
 	pac_color = "yellow";
 	var cnt = 100;
-	var food_remain = 46;
+	var food_remain = 5;
 	var pacman_remain = 1;
+	remainingBalls = food_remain + 1;
 	health = 5;
 	maxMonsters = 4;
 	start_time = new Date();
-	for (var i = 0; i < 10; i++)
+	for (var i = 0; i < rows; i++)
 		board[i] = new Array();
 
 	initMobs();
 
 	var actualFood = 0;
 	
-	for (var i = 0; i < 10; i++) {
+	for (var i = 0; i < rows; i++) {
 		//put obstacles in (i=3,j=3) and (i=3,j=4) and (i=3,j=5), (i=6,j=1) and (i=6,j=2)
-		for (var j = 0; j < 10; j++) {
+		for (var j = 0; j < columns; j++) {
 			if (
 				(i == 0 && j == 3) ||
 				(i == 1 && j == 3) ||
@@ -76,6 +156,64 @@ function Start() {
 				(i == 5 && j == 8) ||
 				(i == 5 && j == 9) ||
 
+				(i == 12 && j == 7) ||
+				(i == 12 && j == 8) ||
+				(i == 12 && j == 9) ||
+
+				(i == 12 && j == 11) ||
+
+				(i == 16 && j == 0) ||
+				(i == 16 && j == 1) ||
+				(i == 16 && j == 2) ||
+				(i == 16 && j == 3) ||
+
+				(i == 9 && j == 1) ||
+				(i == 9 && j == 2) ||
+
+				(i == 21 && j == 11) ||
+				(i == 21 && j == 10) ||
+				(i == 21 && j == 9) ||
+
+
+				(i == 20 && j == 5) ||
+				(i == 20 && j == 6) ||
+				(i == 20 && j == 7) ||
+
+
+				(i == 15 && j == 7) ||
+
+				(i == 17 && j == 7) ||
+				(i == 18 && j == 7) ||
+				(i == 14 && j == 7) ||
+
+				(i == 10 && j == 5) ||
+				(i == 11 && j == 5) ||
+				(i == 12 && j == 5) ||
+
+				(i == 12 && j == 2) ||
+				(i == 12 && j == 3) ||
+				(i == 12 && j == 4) ||
+
+				(i == 7 && j == 8) ||
+				(i == 8 && j == 8) ||
+				(i == 9 && j == 8) ||
+				(i == 10 && j == 8) ||
+				(i == 8 && j == 7) ||
+
+				(i == 22 && j == 2) ||
+				(i == 23 && j == 2) ||
+				(i == 24 && j == 2) ||
+
+				(i == 19 && j == 2) ||
+				(i == 20 && j == 2) ||
+				
+				(i == 23 && j == 3) ||
+				(i == 23 && j == 4) ||
+				(i == 23 && j == 5) ||
+
+
+
+
 				(i == 6 && j == 2)
 			) {
 				board[i][j] = 4;
@@ -84,7 +222,7 @@ function Start() {
 					var randomNum = Math.random();
 					if (randomNum <= (1.0 * food_remain) / cnt) {
 						food_remain--;
-						board[i][j] = 1;
+						board[i][j] = 0;
 						actualFood++;
 					} else if (randomNum < (1.0 * (pacman_remain + food_remain)) / cnt) {
 						shape.i = i;
@@ -104,28 +242,26 @@ function Start() {
 	var healthCell = findRandomEmptyCell(board);
 	board[healthCell[0]][healthCell[1]] = 9;
 
-	while (food_remain > 0) {
-		var emptyCell = findRandomEmptyCell(board);
-		board[emptyCell[0]][emptyCell[1]] = 1;
-		food_remain--;
-		actualFood++;
-	}
-
-
 	var mediumAmount = Math.floor(0.3 * actualFood);
 	var largeAmount = Math.floor(0.1 * actualFood);
 	var cell;
 
 	for (var count = 0; count < mediumAmount; count++)
 	{
-		cell = findRandomFoodCell(board);
+		cell = findRandomEmptyCell(board);
 		board[cell[0]][cell[1]] = 11;
 	}
 	
 	for (var count = 0; count < largeAmount; count++)
 	{
-		cell = findRandomFoodCell(board);
+		cell = findRandomEmptyCell(board);
 		board[cell[0]][cell[1]] = 111;
+	}
+
+	while (actualFood > 0) {
+		var emptyCell = findRandomEmptyCell(board);
+		board[emptyCell[0]][emptyCell[1]] = 1;
+		actualFood--;
 	}
 
 
@@ -145,32 +281,32 @@ function Start() {
 		false
 	);
 	interval = setInterval(UpdatePosition, 100);
-	interval2 = setInterval(moveMobs, 250);
+	interval2 = setInterval(moveMobs, 350);
 }
 
 function findRandomEmptyCell(board) {
-	var i = Math.floor(Math.random() * 9 + 1);
-	var j = Math.floor(Math.random() * 9 + 1);
+	var i = Math.floor(Math.random() * (rows));
+	var j = Math.floor(Math.random() * (columns));
 	while (board[i][j] != 0) {
-		i = Math.floor(Math.random() * 9 + 1);
-		j = Math.floor(Math.random() * 9 + 1);
+		i = Math.floor(Math.random() * (rows));
+		j = Math.floor(Math.random() * (columns));
 	}
 	return [i, j];
 }
 
 function findRandomFoodCell(board) {
-	var i = Math.floor(Math.random() * 9 + 1);
-	var j = Math.floor(Math.random() * 9 + 1);
+	var i = Math.floor(Math.random() * (rows));
+	var j = Math.floor(Math.random() * (columns));
 	while (board[i][j] != 1) {
-		i = Math.floor(Math.random() * 9 + 1);
-		j = Math.floor(Math.random() * 9 + 1);
+		i = Math.floor(Math.random() * (rows));
+		j = Math.floor(Math.random() * (columns));
 	}
 	return [i, j];
 }
 
 function isKeyPressed() {
 
-	if (keysDown[38] || keysDown[40] || keysDown[37] || keysDown[39]) 
+	if (keysDown[up] || keysDown[down] || keysDown[left] || keysDown[right]) 
 		return true;
 
 	return false;
@@ -179,16 +315,16 @@ function isKeyPressed() {
 
 function GetKeyPressed() {
 	
-	if (keysDown[38]) {
+	if (keysDown[up]) {
 		lastKeyPressed = "up";
 	}
-	if (keysDown[40]) {
+	if (keysDown[down]) {
 		lastKeyPressed = "down";
 	}
-	if (keysDown[37]) {
+	if (keysDown[left]) {
 		lastKeyPressed = "left";
 	}
-	if (keysDown[39]) {
+	if (keysDown[right]) {
 		lastKeyPressed = "right";
 	}
 
@@ -198,23 +334,24 @@ function GetKeyPressed() {
 function Draw() {
 	canvas.width = canvas.width; //clean board
 	lblScore.value = score;
+	healthLabel.value = health;
 	lblTime.value = time_elapsed;
-	for (var i = 0; i < 10; i++) {
-		for (var j = 0; j < 10; j++) {
+	for (var i = 0; i < rows; i++) {
+		for (var j = 0; j < columns; j++) {
 			var center = new Object();
-			center.x = i * 60 + 30;
-			center.y = j * 60 + 30;
+			center.x = i * base_size + base_size/2;
+			center.y = j * base_size + base_size/2;
 			if (board[i][j] == 2) {
 				var key = GetKeyPressed();
 				context.beginPath();
 				if (key == "left")
-					context.arc(center.x, center.y, 30, 1.15 * Math.PI, 0.85 * Math.PI); // half circle left
+					context.arc(center.x, center.y, base_size/2, 1.15 * Math.PI, 0.85 * Math.PI); // half circle left
 				else if (key =="right")
-					context.arc(center.x, center.y, 30, 0.15 * Math.PI, 1.85 * Math.PI); // half circle right
+					context.arc(center.x, center.y, base_size/2, 0.15 * Math.PI, 1.85 * Math.PI); // half circle right
 				else if (key =="up")
-					context.arc(center.x, center.y, 30, 0 * Math.PI, 2 * Math.PI); // half circle right
+					context.arc(center.x, center.y, base_size/2, 0 * Math.PI, 2 * Math.PI); // half circle right
 				else if (key =="down")
-					context.arc(center.x, center.y, 30, 0 * Math.PI, 2 * Math.PI); // half circle right
+					context.arc(center.x, center.y, base_size/2, 0 * Math.PI, 2 * Math.PI); // half circle right
 
 
 				context.lineTo(center.x, center.y);
@@ -222,11 +359,11 @@ function Draw() {
 				context.fill();
 				context.beginPath();
 				if (key != "up" && key != "down")
-					context.arc(center.x + 5, center.y - 15, 4, 0, 2 * Math.PI); // circle
+					context.arc(center.x + base_size/12, center.y - base_size/4, base_size/15, 0, 2 * Math.PI); // circle
 				if (key == "down")
 				{
-					context.arc(center.x + 7, center.y - 15, 4, 0, 2 * Math.PI); // circle
-					context.arc(center.x - 7, center.y - 15, 4, 0, 2 * Math.PI); // circle
+					context.arc(center.x + base_size/10, center.y - base_size/4, base_size/15, 0, 2 * Math.PI); // circle
+					context.arc(center.x - base_size/10, center.y - base_size/4, base_size/15, 0, 2 * Math.PI); // circle
 				}
 
 				context.fillStyle = "black"; //color
@@ -289,10 +426,9 @@ function Draw() {
 
 			}
 			else if (board[i][j] == 4) {
-				context.beginPath();
-				context.rect(center.x - 30, center.y - 30, 60, 60);
-				context.fillStyle = "grey"; //color
-				context.fill();
+				base_image = new Image();
+				base_image.src = './resources/wall.jpg';
+				context.drawImage(base_image, center.x - base_size/2, center.y - base_size/2, base_size, base_size);
 			}
 
 			if (i == mob1.i && j == mob1.j)
@@ -317,10 +453,16 @@ function Draw() {
 
 function drawMob(context, center)
 {
+	base_image = new Image();
+	base_image.src = './resources/mob.jpg';
+	context.drawImage(base_image, center.x - base_size/2, center.y - base_size/2, base_size, base_size);
+
+	/*
 	context.beginPath();
 	context.rect(center.x - 30, center.y - 30, 60, 60);
 	context.fillStyle = "red"; //color
 	context.fill();
+	*/
 }
 
 function UpdatePosition() {
@@ -334,7 +476,7 @@ function UpdatePosition() {
 			}
 		}
 		else if (x == "down") {
-			if (shape.j < 9 && board[shape.i][shape.j + 1] != 4) {
+			if (shape.j < columns-1 && board[shape.i][shape.j + 1] != 4) {
 				shape.j++;
 			}
 		}
@@ -344,7 +486,7 @@ function UpdatePosition() {
 			}
 		}
 		else if (x == "right") {
-			if (shape.i < 9 && board[shape.i + 1][shape.j] != 4) {
+			if (shape.i < rows-1 && board[shape.i + 1][shape.j] != 4) {
 				shape.i++;
 			}
 		}
@@ -352,12 +494,15 @@ function UpdatePosition() {
 	
 	if (board[shape.i][shape.j] == 1) {
 		score = score + 5;
+		remainingBalls--;
 	}
 	if (board[shape.i][shape.j] == 11) {
 		score = score + 15;
+		remainingBalls--;
 	}
 	if (board[shape.i][shape.j] == 111) {
 		score = score + 25;
+		remainingBalls--;
 	}
 
 
@@ -397,10 +542,20 @@ function UpdatePosition() {
 		window.clearInterval(interval2);
 		window.alert("Loser!");
 	}
+
+	else if (remainingBalls == 0)
+	{
+		window.clearInterval(interval);
+		window.clearInterval(interval2);
+		Draw();
+		window.alert("Winner!");
+	}
+
 	else if (gameTime <= time_elapsed)
 	{
 		if (score < 100)
 		{
+			lblTime.value = gameTime;
 			window.clearInterval(interval);
 			window.clearInterval(interval2);
 			window.alert("You are better than " + score + " points!");
@@ -439,8 +594,8 @@ function checkMobsPlayerLocations()
 function initMobs()
 {
 
-	for (var i = 0; i < 10; i++)
-		for (var j = 0; j < 10; j++)
+	for (var i = 0; i < rows; i++)
+		for (var j = 0; j < columns; j++)
 			if (board[i][j] == 5)
 				board[i][j] = 0;
 
@@ -454,22 +609,22 @@ function initMobs()
 
 	if (maxMonsters > 1)
 	{
-		board[9][9] = 5;
-		mob2.i = 9;
-		mob2.j = 9;
+		board[rows-1][columns-1] = 5;
+		mob2.i = rows-1;
+		mob2.j = columns-1;
 	}
 	
 	if (maxMonsters > 2)
 	{
-		board[0][9] = 5;
+		board[0][columns-1] = 5;
 		mob3.i = 0;
-		mob3.j = 9;
+		mob3.j = columns-1;
 	}
 
 	if (maxMonsters > 3)
 	{
-		board[9][0] = 5;
-		mob4.i = 9;
+		board[rows-1][0] = 5;
+		mob4.i = rows-1;
 		mob4.j = 0;
 	}
 }
@@ -491,33 +646,86 @@ function moveMobs()
 function moveMob(mob)
 {
 
-	var rndCell = Math.floor(Math.random()*4);
+	var randMove;
+	var moved = false;
+	tries = 0;
 
-	if (rndCell == 0) // up
+	while (!moved && tries < 10)
 	{
-		if (mob.i > 0 && board[mob.i - 1][mob.j] != 4) {
-			mob.i--;
+		randMove = Math.floor(Math.random()*2);
+
+		if (randMove == 0)
+		{
+
+			tries++;
+			if (mob.i > 0 && board[mob.i - 1][mob.j] != 4 && mob.i > shape.i) {
+				mob.i--;
+				moved = true;
+			}
+		
+			else if (mob.i < rows-1 && board[mob.i + 1][mob.j] != 4 && mob.i < shape.i) {
+				mob.i++;
+				moved = true;
+			}
 		}
-	}
 
-	else if (rndCell == 1) // down
-	{
-		if (mob.i < 9 && board[mob.i + 1][mob.j] != 4) {
-			mob.i++;
+		else
+		{
+			tries++;
+			if (mob.j > 0 && board[mob.i][mob.j - 1] != 4 && mob.j > shape.j) {
+				mob.j--;
+				moved = true;
+			}
+		
+			else if (mob.j < columns-1 && board[mob.i][mob.j + 1] != 4 && mob.j < shape.j) {
+				mob.j++;
+				moved = true;
+			}
 		}
-	}
 
-	else if (rndCell == 2) // left
-	{
-		if (mob.j > 0 && board[mob.i][mob.j - 1] != 4) {
-			mob.j--;
-		}
-	}
+		while (!moved)
+		{
+			var rnd = Math.floor(Math.random()*4);
 
-	else if (rndCell == 3) // right
-	{
-		if (mob.j < 9 && board[mob.i][mob.j + 1] != 4) {
-			mob.j++;
+
+			if (rnd == 0)
+			{
+				if (mob.i > 0 && board[mob.i - 1][mob.j] != 4)
+					mob.i--;
+					moved = true;
+				}
+
+			else if (rnd == 1)
+			{
+				if (mob.i < rows-1 && board[mob.i + 1][mob.j] != 4) 
+					mob.i++;
+					moved = true;
+				}
+
+			else if (rnd == 2)
+			{
+				if (mob.j > 0 && board[mob.i][mob.j - 1] != 4) 
+					mob.j--;
+					moved = true;
+
+				}
+			else
+			{
+
+				if (mob.j < columns-1 && board[mob.i][mob.j + 1] != 4) 
+					mob.j++;
+					moved = true;
+
+			}
+
+
+
+
+
+
+
+			
+
 		}
 	}
 
